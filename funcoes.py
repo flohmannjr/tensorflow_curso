c73e10ea0f27e5021f89244d4ce1186a16f1ce99
Frederico Lohmann Jr <flohmannjr@yahoo.com>
2023-01-26 09:42:47 -0300

    Update load_and_prep_image

--- funcoes.py
+++ funcoes.py
@@ -89,26 +89,28 @@ def grafico_historicos_complementares(original, complementar):
     plt.legend(frameon=True, facecolor='white')
     plt.show()
 
-# Create a function to import and image and resize it to be able to be used with our model
-def load_and_prep_image(filename, img_shape=224):
+# Create a function to import an image and resize it to be able to be used with our model
+def load_and_prep_image(filename, img_shape=224, scale=True):
     """
-    Reads an image from filename, turns it into a tensor and reshapes it 
-    to (img_shape, img_shape, colour_channels).
+    Reads in an image from filename, turns it into a tensor and reshapes into
+    (224, 224, 3).
+    Parameters
+    ----------
+    filename (str): string filename of target image
+    img_shape (int): size to resize target image to, default 224
+    scale (bool): whether to scale pixel values to range(0, 1), default True
     """
-
     # Read in the image
     img = tf.io.read_file(filename)
-
-    # Decode the read file into a tensor
-    img = tf.image.decode_image(img)
-
+    # Decode it into a tensor
+    img = tf.image.decode_jpeg(img)
     # Resize the image
-    img = tf.image.resize(img, size=[img_shape, img_shape])
-
-    # Rescale the image (get all values between 0 and 1)
-    img = img/255.
-
-    return img
+    img = tf.image.resize(img, [img_shape, img_shape])
+    if scale:
+        # Rescale the image (get all values between 0 and 1)
+        return img/255.
+    else:
+        return img
 
 def pred_and_plot(model, filename, class_names):
     """
