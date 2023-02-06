cd0c877abb74f69be54a2fe132dab06c5711202a
Frederico Lohmann Jr <flohmannjr@yahoo.com>
2023-02-05 23:03:18 -0300

    Update funcoes_temporais.py

--- funcoes_temporais.py
+++ funcoes_temporais.py
@@ -8,6 +8,8 @@ import os
 
 from sklearn.metrics import mean_absolute_error, mean_squared_error, mean_absolute_percentage_error
 
+from tensorflow.keras.callbacks import ModelCheckpoint
+
 COR_TREINO   = '#663399'
 COR_PREVISAO = '#f22424'
 COR_TESTE    = '#345cd3'
